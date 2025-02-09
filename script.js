33cf0cda58934d37bceca77780c4edca99ebeed2
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-09 12:22:54 -0700

    Add Buttons To Calculator
    Added the number buttons and operator buttons to
    the calculator row containers.

--- script.js
+++ script.js
@@ -16,13 +16,71 @@ for (let i = 0; i < 7; i++) {
 expression = document.createElement("div");
 expression.classList.add("expression");
 expression.textContent = "1 + 2";
-rows[0].appendChild(expression);
 
 // Create text box for input
-result = document.createElement("div");
+const result = document.createElement("div");
 result.classList.add("result");
 result.textContent = "3";
+
+// Create number buttons
+const numbers = [];
+for (let num = 0; num < 10; num++) {
+  numbers[num] = document.createElement("button");
+  numbers[num].classList.add("number");
+  numbers[num].textContent = num;
+}
+
+// Create Operators
+plusButton = document.createElement("button");
+plusButton.classList.add("operator");
+plusButton.textContent = "+";
+
+minusButton = document.createElement("button");
+minusButton.classList.add("operator");
+minusButton.textContent = "-";
+
+multiplicationButton = document.createElement("button");
+multiplicationButton.classList.add("operator");
+multiplicationButton.textContent = "x";
+
+divisionButton = document.createElement("button");
+divisionButton.classList.add("operator");
+divisionButton.textContent = "÷";
+
+equalsButton = document.createElement("button");
+equalsButton.classList.add("operator");
+equalsButton.textContent = "=";
+
+// Build row 1
+rows[0].appendChild(expression);
+
+// Build row 2
 rows[1].appendChild(result);
+
+// Build row 3
+rows[2].appendChild(divisionButton);
+
+// Build row 4
+rows[3].appendChild(numbers[7]);
+rows[3].appendChild(numbers[8]);
+rows[3].appendChild(numbers[9]);
+rows[3].appendChild(multiplicationButton);
+
+// Build row 5
+rows[4].appendChild(numbers[4]);
+rows[4].appendChild(numbers[5]);
+rows[4].appendChild(numbers[6]);
+rows[4].appendChild(minusButton);
+
+// Build row 6
+rows[5].appendChild(numbers[1]);
+rows[5].appendChild(numbers[2]);
+rows[5].appendChild(numbers[3]);
+rows[5].appendChild(plusButton);
+
+// Build row 7
+rows[6].appendChild(numbers[0]);
+rows[6].appendChild(equalsButton);
 
 function add(a, b) {
   return a + b;
