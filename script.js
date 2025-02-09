b95242249066683e64e31b339f007e4aba4a0ab3
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-09 10:35:16 -0700

    Update Script For Initial Layout
    Updated the script to create the rows for
    components. Assigned first two rows to be the
    expression and result.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+// Main container for the calculator
+const calculator = document.querySelector(".container");
+
+// Create rows to house buttons and text boxes
+const rows = [];
+for (let i = 0; i < 7; i++) {
+  rows[i] = document.createElement("div");
+  rows[i].classList.add("row");
+  calculator.appendChild(rows[i]);
+}
+
+// Create text box for input
+expression = document.createElement("div");
+expression.classList.add("expression");
+expression.textContent = "1 + 2";
+rows[0].appendChild(expression);
+
+// Create text box for input
+result = document.createElement("div");
+result.classList.add("result");
+result.textContent = "3";
+rows[1].appendChild(result);
