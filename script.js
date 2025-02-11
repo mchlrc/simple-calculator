0aa615b64241f87b75d3ada83a87033fdd7014d6
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-10 18:27:30 -0700

    Add Functionality To Buttons
    Added functionality to the buttons and added
    logic to calculate the results. This is a near
    feature-complete calculator.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Main container for the calculator
 const calculator = document.querySelector(".container");
-let a = [];
-let b = [];
+let left = [];
+let right = [];
 let operator = "";
+let result = 0;
 
 // Create rows to house buttons and text boxes
 const rows = [];
@@ -18,16 +19,16 @@ expression.classList.add("expression");
 expression.textContent = "";
 
 // Create text box for input
-const result = document.createElement("div");
-result.classList.add("result");
-result.textContent = "";
+const resultBox = document.createElement("div");
+resultBox.classList.add("result");
+resultBox.textContent = "0";
 
 // Create number buttons
-const numbers = [];
+const numberButtons = [];
 for (let num = 0; num < 10; num++) {
-  numbers[num] = document.createElement("button");
-  numbers[num].classList.add("number");
-  numbers[num].textContent = num;
+  numberButtons[num] = document.createElement("button");
+  numberButtons[num].classList.add("number");
+  numberButtons[num].textContent = num;
 }
 
 decimalButton = document.createElement("button");
@@ -35,21 +36,26 @@ decimalButton.classList.add("number");
 decimalButton.textContent = ".";
 
 // Create Operators
+const operatorButtons = [];
 plusButton = document.createElement("button");
 plusButton.classList.add("operator");
 plusButton.textContent = "+";
+operatorButtons.push(plusButton);
 
 minusButton = document.createElement("button");
 minusButton.classList.add("operator");
 minusButton.textContent = "-";
+operatorButtons.push(minusButton);
 
 multiplicationButton = document.createElement("button");
 multiplicationButton.classList.add("operator");
 multiplicationButton.textContent = "x";
+operatorButtons.push(multiplicationButton);
 
 divisionButton = document.createElement("button");
 divisionButton.classList.add("operator");
 divisionButton.textContent = String.fromCharCode(247); // Divide symbol
+operatorButtons.push(divisionButton);
 
 equalsButton = document.createElement("button");
 equalsButton.classList.add("operator");
@@ -69,7 +75,7 @@ allClearButton.textContent = "AC";
 rows[0].appendChild(expression);
 
 // Build row 2
-rows[1].appendChild(result);
+rows[1].appendChild(resultBox);
 
 // Build row 3
 rows[2].appendChild(deleteButton);
@@ -77,27 +83,82 @@ rows[2].appendChild(allClearButton);
 rows[2].appendChild(divisionButton);
 
 // Build row 4
-rows[3].appendChild(numbers[7]);
-rows[3].appendChild(numbers[8]);
-rows[3].appendChild(numbers[9]);
+rows[3].appendChild(numberButtons[7]);
+rows[3].appendChild(numberButtons[8]);
+rows[3].appendChild(numberButtons[9]);
 rows[3].appendChild(multiplicationButton);
 
 // Build row 5
-rows[4].appendChild(numbers[4]);
-rows[4].appendChild(numbers[5]);
-rows[4].appendChild(numbers[6]);
+rows[4].appendChild(numberButtons[4]);
+rows[4].appendChild(numberButtons[5]);
+rows[4].appendChild(numberButtons[6]);
 rows[4].appendChild(minusButton);
 
 // Build row 6
-rows[5].appendChild(numbers[1]);
-rows[5].appendChild(numbers[2]);
-rows[5].appendChild(numbers[3]);
+rows[5].appendChild(numberButtons[1]);
+rows[5].appendChild(numberButtons[2]);
+rows[5].appendChild(numberButtons[3]);
 rows[5].appendChild(plusButton);
 
 // Build row 7
-rows[6].appendChild(numbers[0]);
+rows[6].appendChild(numberButtons[0]);
 rows[6].appendChild(decimalButton);
 rows[6].appendChild(equalsButton);
+
+// Add event listeners to numbers
+for (let num of numberButtons) {
+  num.addEventListener("click", (event) => {
+    if (operator === "") {
+      left.push(event.target.textContent);
+      updateExpression();
+    } else {
+      right.push(event.target.textContent);
+      updateExpression();
+    }
+  });
+}
+
+for (let operatorButton of operatorButtons) {
+  operatorButton.addEventListener("click", (event) => {
+    if (operator === "") {
+      operator = event.target.textContent;
+    }
+    updateExpression();
+  });
+}
+
+// Add event listener to decimal button
+decimalButton.addEventListener("click", (event) => {
+  if (operator === "") {
+    if (!left.includes(".")) {
+      left.push(event.target.textContent);
+      updateExpression();
+    }
+  } else {
+    if (!right.includes(".")) {
+      right.push(event.target.textContent);
+      updateExpression();
+    }
+  }
+});
+
+// Add event listener to equals button
+equalsButton.addEventListener("click", () => {
+  if (left.length > 0 && right.length > 0 && operator.length > 0) {
+    result = operate(left, right, operator);
+    updateResult();
+  }
+});
+
+// Add event listener to clear all button
+allClearButton.addEventListener("click", () => {
+  clearAll();
+});
+
+// Add event listener to delete button
+deleteButton.addEventListener("click", () => {
+  backSpace();
+});
 
 function add(a, b) {
   return a + b;
@@ -113,25 +174,76 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-    return "DIVBY0";
+    return "DIVBYZERO";
   } else {
     return a / b;
   }
 }
 
 function operate(a, b, operator) {
-  let result = "";
+  a = Number.parseFloat(a.join(""));
+  b = Number.parseFloat(b.join(""));
   switch (operator) {
     case "+":
-      result = add(a, b);
+      return add(a, b);
     case "-":
-      result = subtract(a, b);
+      return subtract(a, b);
     case "x":
-      result = multiply(a, b);
-    case "÷":
-      result = divide(a, b);
+      return multiply(a, b);
+    case String.fromCharCode(247): // Divide
+      return divide(a, b);
     default:
-      result = "ERROR";
+      return "ERROR";
   }
-  return result;
+}
+
+function updateExpression() {
+  let leftHand = "";
+  if (left.length !== 0) {
+    leftHand = left.join("");
+  }
+
+  let rightHand = "";
+  if (right.length !== 0) {
+    rightHand = right.join("");
+  }
+  leftHand = formatNumber(leftHand, 7);
+  rightHand = formatNumber(rightHand, 7);
+  expression.textContent = `${leftHand} ${operator} ${rightHand}`;
+}
+
+function updateResult() {
+  resultBox.textContent = `${formatNumber(result, 10)}`;
+}
+
+function formatNumber(num, length) {
+  if (num.toString().length > length) {
+    return Number.parseFloat(num).toExponential(2);
+  } else {
+    return num;
+  }
+}
+
+function clearAll() {
+  while (left.length > 0) {
+    left.shift(1);
+  }
+  while (right.length > 0) {
+    right.shift(1);
+  }
+  operator = "";
+  updateExpression();
+  result = 0;
+  updateResult();
+}
+
+function backSpace() {
+  if (right.length > 0) {
+    right.shift(1);
+  } else if (operator !== "") {
+    operator = "";
+  } else if (left.length > 0) {
+    left.shift(1);
+  }
+  updateExpression();
 }
